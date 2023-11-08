d938bfa6abdcde9e3697ac8c713d76bb9dc64114
hewmx <mxhew20@gmail.com>
2023-11-09 03:52:36 +1300

    Removed redundant read_test.py and schedule_read.py

--- read/apps.py
+++ read/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class ReadConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'read'
-
-    #def ready(self):
-        #from . import schedule_read
-        #schedule_read.start()
