0671106a90ba672068fe8c5554db02343b76c0fb
hewmx <mxhew20@gmail.com>
2023-11-08 16:38:23 +1300

    Added Knox authetication to settings.py Remove ready() function from apps.py

--- read/apps.py
+++ read/apps.py
@@ -5,6 +5,6 @@ class ReadConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'read'
 
-    def ready(self):
-        from . import schedule_read
-        schedule_read.start()
+    #def ready(self):
+        #from . import schedule_read
+        #schedule_read.start()
